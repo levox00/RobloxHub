b9daad72d815f2344d4c75c6b166205c0df156f1
Nik <schoneniklas79@gmail.com>
2026-07-17 16:15:55 +0200

    Update loadstrings.lua

--- loadstrings.lua
+++ loadstrings.lua
@@ -1,18 +1,6 @@
 return {
 	{
 		Name = "Grow a Garden 2",
-		Url = "https://raw.githubusercontent.com/levox00/RobloxHub/main/Games/GrowAGarden2.lua",
-	},
-	{
-		Name = "Adopt Me",
-		Url = "https://raw.githubusercontent.com/levox00/RobloxHub/main/Games/AdoptMe.lua",
-	},
-	{
-		Name = "Blox Fruits",
-		Url = "https://raw.githubusercontent.com/levox00/RobloxHub/main/Games/BloxFruits.lua",
-	},
-	{
-		Name = "Jailbreak",
-		Url = "https://raw.githubusercontent.com/levox00/RobloxHub/main/Games/Jailbreak.lua",
-	},
+		Url = "https://raw.githubusercontent.com/levox00/RobloxHub/refs/heads/main/Games/grow-a-garden-2.txt",
+	}
 }
