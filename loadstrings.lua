8b7282ae8fc8e418102e6f70c5ad19884068af6c
Apothecy <leander@rottwinkel.de>
2026-07-17 16:20:13 +0200

    Fix loadstrings.lua to point to main branch

--- loadstrings.lua
+++ loadstrings.lua
@@ -1,18 +1,18 @@
 return {
 	{
 		Name = "Grow a Garden 2",
-		Url = "https://raw.githubusercontent.com/levox00/RobloxHub/game/gag2-hub-integration/Games/GrowAGarden2.lua",
+		Url = "https://raw.githubusercontent.com/levox00/RobloxHub/main/Games/GrowAGarden2.lua",
 	},
 	{
 		Name = "Adopt Me",
-		Url = "https://raw.githubusercontent.com/levox00/RobloxHub/game/gag2-hub-integration/Games/AdoptMe.lua",
+		Url = "https://raw.githubusercontent.com/levox00/RobloxHub/main/Games/AdoptMe.lua",
 	},
 	{
 		Name = "Blox Fruits",
-		Url = "https://raw.githubusercontent.com/levox00/RobloxHub/game/gag2-hub-integration/Games/BloxFruits.lua",
+		Url = "https://raw.githubusercontent.com/levox00/RobloxHub/main/Games/BloxFruits.lua",
 	},
 	{
 		Name = "Jailbreak",
-		Url = "https://raw.githubusercontent.com/levox00/RobloxHub/game/gag2-hub-integration/Games/Jailbreak.lua",
+		Url = "https://raw.githubusercontent.com/levox00/RobloxHub/main/Games/Jailbreak.lua",
 	},
 }
